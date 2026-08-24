6e529fd0b9f19af9de04351359ae74c3d19612d4
Thomas <t.hamilton0416@gmail.com>
2026-08-23 19:42:38 -0500

    Add TypeScript to stack

--- src/data/projects.js
+++ src/data/projects.js
@@ -54,6 +54,6 @@ export const sections = [
   { id: 'thanks', label: 'THANKS' },
 ];
 
-export const stack = ['Python', 'React', 'Durable Objects'];
+export const stack = ['TypeScript', 'Python', 'React', 'Durable Objects'];
 
 export const skills = ['Java', 'JavaScript', 'Python', 'C++', 'Dart', 'HTML / CSS', 'Git', 'Agile SDLC', 'CI/CD'];
